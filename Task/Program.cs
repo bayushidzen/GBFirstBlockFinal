2fe36ebb862acc78a5c34c18e36f4fa1e3c156b4
bayushidzen <zhek_wh@mail.ru>
2022-12-25 23:47:50 +0300

    Coding task

---
+++ Task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+string[] arr1 = {"hello","2","world",":-)"};
+string[] arr2 = new string[arr1.Length-1];
+int j = 0;
+
+for (int i=0; i< arr1.Length;i++)
+{
+    if (arr1[i].Length <= 3) 
+    {
+        arr2[j] = arr1[i];
+        Console.WriteLine(arr2[j]);
+        j++;
+    }
+}
